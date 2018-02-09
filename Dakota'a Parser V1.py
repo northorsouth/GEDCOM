7e782d2c50633b78ad6ca2986fd801d5d9797d9e
nestouranitsas <nestouranitsas@gmail.com>
2018-02-09 18:00:41 -0500

    Added Dakota's Parser to file and table

--- Dakota'a Parser V1.py
+++ Dakota'a Parser V1.py
@@ -22,73 +22,68 @@ tagRules =[
 
 # if a file name was passed in
 if len(sys.argv) > 1:
-	
+
 	indID = ""
 	name = ""
 	gender = ""
 	birth = ""
 	death = "None"
-	
+
 	lastTag = None
-	
+
 	# loop through lines
 	for line in open(sys.argv[1]):
-		
+
 		valid = False
 		level = -1
 		tag = None
 		args = None
-		
+
 		# make sure this line has at least a level and a tag
 		words = line.split()
 		if len(words)>=2:
-			
+
 			# level is always first
 			level = int(words[0])
-			
+
 			# flag for INDI and FAM
 			badOrder = False
-			
+
 			# order is switched for INDI and FAM
 			if (len(words)>=3 and
 				(words[2]=='INDI' or words[2]=='FAM')):
-				
+
 				tag = words[2]
 				args = " ".join([words[1]] + words[3:])
-			
 			else:
 				tag = words[1]
 				args = " ".join(words[2:])
-				
+
 				# INDI and FAM should have been found in block above
 				# if this trips, the order is wrong
 				if tag=='INDI' or tag=='FAM':
 					badOrder = True
-			
+
 			# guilty until proven innocent
 			valid = False
-			
+
 			# if we find a matching tag rule, and the level checks out
 			# tag is valid
 			if not badOrder:
 				for tagRule in tagRules:
 					if tagRule[1]==tag and tagRule[0]==level:
 						valid = True
-			
+
 			if (tag == 'NAME'):
 				name = args
-				
 			elif (tag == "SEX"):
 				gender = args[0]
-			
 			elif (lastTag=="BIRT" and tag == "DATE"):
 				birth = args
-			
 			elif (lastTag=="DEAT" and tag == "DATE"):
 				birth = args
-			
+
 			lastTag = tag
-		
+
 		if (valid and level==0):
-			
 			print("" + name + ", " + gender + ", " + birth + ", " + death)
--- GEDCOM Parser.py
+++ GEDCOM Parser.py
@@ -1,19 +1,141 @@
 import os
 import sqlite3
 import sys
+import PrettyTable
 
 dbName = "GEDCOM.db"
 
+tagRules =[
+	(0, 'INDI'),
+	(1, 'NAME'),
+	(1, 'SEX'),
+	(1, 'BIRT'),
+	(1, 'DEAT'),
+	(1, 'FAMC'),
+	(1, 'FAMS'),
+	(0, 'FAM'),
+	(1, 'MARR'),
+	(1, 'HUSB'),
+	(1, 'WIFE'),
+	(1, 'CHIL'),
+	(1, 'DIV'),
+	(2, 'DATE'),
+	(0, 'HEAD'),
+	(0, 'TRLR'),
+	(0, 'NOTE')
+]
+
+# if a file name was passed in
+if len(sys.argv) > 1:
+
+	indID = ""
+	name = ""
+	gender = ""
+	birth = ""
+	death = "N/A"
+
+	famID = ""
+	husband = ""
+	wife = ""
+	child = ""
+	married = ""
+	divorced = "N/A"
+
+	lastTag = None
+
+	# loop through lines
+	for line in open(sys.argv[1]):
+
+		valid = False
+		level = -1
+		tag = None
+		args = None
+
+		# make sure this line has at least a level and a tag
+		words = line.split()
+		if len(words) >= 2:
+
+			# level is always first
+			level = int(words[0])
+
+			# flag for INDI and FAM
+			badOrder = False
+
+			# order is switched for INDI and FAM
+			if (len(words) >= 3 and
+				(words[2] == 'INDI' or words[2] == 'FAM')):
+
+				tag = words[2]
+				args = " ".join([words[1]] + words[3:])
+			else:
+				tag = words[1]
+				args = " ".join(words[2:])
+
+				# INDI and FAM should have been found in block above
+				# if this trips, the order is wrong
+				if tag == 'INDI' or tag == 'FAM':
+					badOrder = True
+
+			# guilty until proven innocent
+			valid = False
+
+			# if we find a matching tag rule, and the level checks out
+			# tag is valid
+			if not badOrder:
+				for tagRule in tagRules:
+					if tagRule[1]==tag and tagRule[0]==level:
+						valid = True
+
+			if (tag == 'NAME'):
+				name = args
+			elif (tag == 'SEX'):
+				gender = args[0]
+			elif (lastTag == 'BIRT' and tag == 'DATE'):
+				birth = args
+			elif (lastTag == 'DEAT' and tag == 'DATE'):
+				birth = args
+
+		#prints indiviual's info into table
+		if (valid and level == 0):
+
+			#Table for individuals
+			INDI_tbl = PrettyTable()
+			INDI_tbl.fieldnames = ["ID","Name"]
+
+			INDI_tbl.add_row([name, gender, birth, death])
+			print(INDI_tbl)
+
+		#prints familiy	info into table
+		if (tag == 'HUSB'):
+				husband = args
+			elif (tag == 'WIFE'):
+				wife = args
+			elif (tag == 'CHIL'):
+				child = args
+			elif (lastTag == 'MARR' and tagg == 'DATE'):
+				married = args
+			elif (lastTag == 'DIV' and tag == 'DATE'):
+				divorced = args
+
+		if (valid and level == 0):
+
+			#Table for families
+			FAM_tbl = PrettyTable()
+			FAM_tbl.fieldnames = ["Family ID","Husband Name","Husband ID","Wife Name","Wife ID"]
+
+			FAM_tbl.add_row([famID, husband, wife])
+			print(FAM_tbl)
+
 def dbInit():
-	
+
 	try:
 		os.remove(dbName);
 	except FileNotFoundError:
 		pass
-	
+
 	conn = sqlite3.connect(dbName)
 	curs = conn.cursor()
-	
+
 	curs.execute('''CREATE TABLE individuals (
 		id 			TEXT	NOT NULL	PRIMARY KEY,
 		firstName	TEXT	NOT NULL,
@@ -21,36 +143,37 @@ def dbInit():
 		gender 		TEXT	NOT NULL,
 		birth 		DATE	NOT NULL,
 		death 		DATE,
-		
+
 		CHECK (gender in ("M", "F"))
 	)''')
-	
+
 	curs.execute('''CREATE TABLE families (
 		id 			TEXT	NOT NULL	PRIMARY KEY,
 		married		DATE	NOT NULL,
 		divorced 	DATE,
 		husbID 		TEXT	NOT NULL,
 		wifeID 		TEXT	NOT NULL,
-		
+
 		FOREIGN KEY (husbID) REFERENCES individuals(id)
 		FOREIGN KEY (wifeID) REFERENCES individuals(id)
 	)''')
-	
+
 	curs.execute('''CREATE TABLE children (
 		indID		TEXT	NOT NULL,
 		famID		TEXT	NOT NULL,
-		
+
 		PRIMARY KEY (indID, famID),
-		
+
 		FOREIGN KEY (indID) REFERENCES individuals(id),
 		FOREIGN KEY (famID) REFERENCES families(id)
 	)''')
-	
+
 	conn.commit()
-	
+
 	return conn
 
 def addIndividual (idStr, firstName, lastName, gender, birth, death):
+<<<<<<< Updated upstream
 	
 	result = True
 	
@@ -64,6 +187,14 @@ def addIndividual (idStr, firstName, lastName, gender, birth, death):
 		print("Couldn't add individual " + idStr + ": " + str(err))
 		result = False 
 	
+=======
+
+	conn.cursor().execute(
+		'INSERT INTO individuals VALUES (?, ?, ?, ?, ?, ?)',
+		(idStr, firstName, lastName, gender, birth, death)
+	)
+
+>>>>>>> Stashed changes
 	conn.commit()
 	
 	return result
@@ -71,6 +202,7 @@ def addIndividual (idStr, firstName, lastName, gender, birth, death):
 
 
 def addFamily (idStr, married, divorced, husbID, wifeID):
+<<<<<<< Updated upstream
 	
 	result = True
 	
@@ -84,6 +216,14 @@ def addFamily (idStr, married, divorced, husbID, wifeID):
 		print("Couldn't add family " + idStr + ": " + str(err))
 		result = False 
 	
+=======
+
+	conn.cursor().execute(
+		'INSERT INTO families VALUES (?, ?, ?, ?, ?)',
+		(idStr, married, divorced, husbID, wifeID)
+	)
+
+>>>>>>> Stashed changes
 	conn.commit()
 	
 	return result
@@ -91,6 +231,7 @@ def addFamily (idStr, married, divorced, husbID, wifeID):
 
 
 def addChild (childID, famID):
+<<<<<<< Updated upstream
 	
 	result = True
 	
@@ -104,6 +245,14 @@ def addChild (childID, famID):
 		print("Couldn't add child " + childID + ": " + str(err))
 		result = False 
 	
+=======
+
+	conn.cursor().execute(
+		'INSERT INTO children VALUES (?, ?)',
+		(childID, famID)
+	)
+
+>>>>>>> Stashed changes
 	conn.commit()
 	
 	return result
@@ -111,13 +260,13 @@ def addChild (childID, famID):
 
 
 def getIndividuals():
-	
+
 	return conn.cursor().execute('SELECT * FROM INDIVIDUALS ORDER BY id').fetchall()
 
 
 
 def getIndividual(indID):
-	
+
 	return conn.cursor().execute(
 		'SELECT * FROM INDIVIDUALS WHERE id=?',
 		(indID,)
@@ -126,13 +275,13 @@ def getIndividual(indID):
 
 
 def getFamilies():
-	
+
 	return conn.cursor().execute('SELECT * FROM FAMILIES ORDER BY id').fetchall()
 
 
 
 def getFamily(famID):
-	
+
 	return conn.cursor().execute(
 		'SELECT * FROM FAMILIES WHERE id=?',
 		(famID,)
