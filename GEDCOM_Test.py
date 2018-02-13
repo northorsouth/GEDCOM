14bf16b194c572fbcabc17098710c3bfbabf0763
nestourantisas <nestouranitsas@gmail.com>
2018-02-13 18:10:49 -0500

    framework for test suite started -- tests still need to be defined minor updates to team report

--- GEDCOM_Test.py
+++ GEDCOM_Test.py
@@ -1,1 +1,21 @@
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+#|Author(s): Nestor Ouranitsas, Dakota Crouchelli                                    |#
+#|This file runs automated tests on the GEDCOM_Parser.py file                        |#
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
+import unittest
+
+#all tests dealing with dates
+class datesTest(unittest.TestCase):
+
+    # validates all dates are before the current date
+    def test_dateBFORcurrent(self):
+
+    # validates the individual is born before marriage
+    def test_birtBFORmarr(self):
+
+    # validates the individual is born before their death date
+    def test_birtBFORdeat(self):
+
+    # validates that a couple was married before they got divorced
+    def test_marrBFORdiv(self):
